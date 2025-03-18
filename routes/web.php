6a9a7b6a4b0f298c873b2362567040fbe5af675f
shakeeb <shakee.zats@gmail.com>
2025-03-18 10:20:15 +0530

    fixed routing

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,21 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\TenantController;
 
-Route::get('/s', function () {
-    return Inertia::render('Welcome');
-})->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('entities', [TenantController::class, 'index'])->name('tenants.index')->middleware(['auth', 'verified']);
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            return Inertia::render('Welcome');
+        })->name('home');
+        
+        Route::get('dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+        
+        Route::get('entities', [TenantController::class, 'index'])->name('tenants.index')->middleware(['auth', 'verified']);
+    });
+}
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
